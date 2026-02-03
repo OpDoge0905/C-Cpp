50f04d5f22f95414bdca89d66a5dbb2375e5c1d6
Duckyhehe <43910762+OpDoge0905@users.noreply.github.com>
2026-02-03 16:02:01 +0000

    Add result files and initial implementation of quickSort algorithm
    - Created Input4_result.txt, test1_result.txt, test2_result.txt, test3_result.txt, and test4_result.txt with respective output data.
    - Added memo.cpp with a commented-out implementation of the quickSort algorithm for sorting data.

---
+++ sheshe/Q3/memo.cpp
@@ -0,0 +1,17 @@
+// void quickSortName(vector<Data> &arr, int start, int end){
+//     // base case
+//     if (start >= end)
+//     {
+//         return;
+//     }
+        
+//     // partitioning the array
+//     int p = partition(arr, start, end);
+ 
+//     // Sorting the left part
+//     quickSortName(arr, start, p - 1);
+ 
+//     // Sorting the right part
+//     quickSortName(arr, p + 1, end);
+
+// }
